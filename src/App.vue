83139715daf5c46fd9e7af35b819a8af93a4217e
杨浩冰 <3195399130@qq.com>
2023-08-06 00:09:14 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Code></Code>
+  </div>
+</template>
+
+<script>
+import Code from "./components/xiaochengxu.vue";
+export default {
+  name: "App",
+  components: {
+    Code
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
